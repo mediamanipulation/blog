622feb2c863669c1db8e89ac9a40b7ba09e4355e
mediamanipulation <manipulation2000@hotmail.com>
2020-01-04 07:06:42 -0600

    refactor Userheader mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchUser} from '../actions'
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {  }
-    // }
-    componentDidMount(){
-        this.props.fetchUser(this.props.userId);
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {  }
+  // }
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+  render() {
+    const { user } = this.props;
+    if (!user) {
+      return null;
     }
-    render() { 
-        const user = this.props.users.find((user) => user.id === this.props.userId );
-        if(!user){
-            return null;
-        }
-        return ( <div><h3>{user.name}</h3></div>  );
-    }
+    return (
+      <div>
+        <h3>{user.name}</h3>
+      </div>
+    );
+  }
 }
-const mapStateToProps = (state) => {
-     return {
-         users: state.users 
-        //  prop: state.prop
-     };
- }
-export default connect(mapStateToProps, { fetchUser }) (UserHeader) ;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.users.find(user => user.id === ownProps.userId)
+  };
+};
+export default connect(
+    mapStateToProps, 
+    { fetchUser }
+    )(UserHeader);
