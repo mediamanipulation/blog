b4e5ad0629d75e43ac68f2c7d12b2bc9785a787d
mediamanipulation <manipulation2000@hotmail.com>
2020-01-05 14:14:15 -0600

    action creators  in action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,34 @@
+// import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log('about to fetch posts')
+  await dispatch(fetchPosts());
+  console.log('fetched posts!')
+
+}
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
- // const id = '4'; 
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-  // console.log(`payload user ${JSON.stringify(response.data)}`)
+ 
 };
 
+//***** memoized  version */
+// export const fetchUser = (id) =>  dispatch => {
+//   _fetchUser(id, dispatch);
 
+// };
+
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
 
 
  // console.log(`payload ${JSON.stringify(response.data)}`)
