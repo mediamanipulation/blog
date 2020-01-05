5251c643b24ffa40aab17b5a9cc92930b07d4a94
mediamanipulation <manipulation2000@hotmail.com>
2020-01-05 15:18:43 -0600

    cleaned up comments

--- src/components/UserHeader_Comments.js
+++ src/components/UserHeader_Comments.js
@@ -1,7 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+// import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {  }
+  // }
+  // not needed now
+  // componentDidMount() {
+  //   this.props.fetchUser(this.props.userId);
+  // }
   render() {
     const { user } = this.props;
     if (!user) {
@@ -19,4 +28,7 @@ const mapStateToProps = (state, ownProps) => {
     user: state.users.find(user => user.id === ownProps.userId)
   };
 };
-export default connect(mapStateToProps)(UserHeader);
+export default connect(
+    mapStateToProps, 
+    // { fetchUser }
+    )(UserHeader);
